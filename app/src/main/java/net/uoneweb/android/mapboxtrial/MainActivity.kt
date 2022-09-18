5ec7c7f81ac5d64bb67fd4489661b9fc99a09b46
u-one <bacflip270bb@gmail.com>
2022-09-18 10:49:40 +0900

    setup mapbox

--- app/src/main/java/net/uoneweb/android/mapboxtrial/MainActivity.kt
+++ app/src/main/java/net/uoneweb/android/mapboxtrial/MainActivity.kt
@@ -2,10 +2,42 @@ package net.uoneweb.android.mapboxtrial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
 
 class MainActivity : AppCompatActivity() {
+
+    private var mapView: MapView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapView = findViewById(R.id.mapView)
+        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+
+
     }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
+    }
+
+
 }
