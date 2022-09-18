416b09b875610aa1143be3360bc569c9eb8cac45
Yuichi Watanabe <yuiwatan@yahoo-corp.jp>
2022-09-18 12:42:18 +0900

    Add viewport plugin

--- app/src/main/java/net/uoneweb/android/mapboxtrial/MainActivity.kt
+++ app/src/main/java/net/uoneweb/android/mapboxtrial/MainActivity.kt
@@ -1,21 +1,54 @@
 package net.uoneweb.android.mapboxtrial
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.EdgeInsets
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
+import com.mapbox.maps.plugin.locationcomponent.location
+import com.mapbox.maps.plugin.viewport.data.FollowPuckViewportStateBearing
+import com.mapbox.maps.plugin.viewport.data.FollowPuckViewportStateOptions
+import com.mapbox.maps.plugin.viewport.state.FollowPuckViewportState
+import com.mapbox.maps.plugin.viewport.viewport
 
 class MainActivity : AppCompatActivity() {
 
     private var mapView: MapView? = null
+
+    private val onIndicatorBearingChangedListener = OnIndicatorBearingChangedListener { bearing ->
+        mapView?.getMapboxMap()?.setCamera(CameraOptions.Builder().bearing(bearing).build())
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         mapView = findViewById(R.id.mapView)
-        mapView?.getMapboxMap()?.loadStyleUri(Style.MAPBOX_STREETS)
+        mapView?.let {
+            initializeMapView(it)
+        }
+    }
 
+    fun initializeMapView(mapView: MapView) {
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS) {
+            mapView.location.updateSettings {
+                enabled = true
+            }
+        }
+
+        val viewportPlugin = mapView?.viewport
+        val followPuckViewportState: FollowPuckViewportState =
+            viewportPlugin.makeFollowPuckViewportState(
+                FollowPuckViewportStateOptions.Builder()
+                    .bearing(FollowPuckViewportStateBearing.Constant(0.0))
+                    .padding(EdgeInsets(400.0 * resources.displayMetrics.density, 0.0, 0.0, 0.0))
+                    .build()
+            )
+
+        viewportPlugin.transitionTo(followPuckViewportState) { success ->
+        }
 
     }
 
@@ -38,6 +71,5 @@ class MainActivity : AppCompatActivity() {
         super.onDestroy()
         mapView?.onDestroy()
     }
-
 
 }
