ea2b67f7ee29f8e980e7f5d89a07d6a1cb7cde88
u-one <bacflip270bb@gmail.com>
2022-09-19 14:53:52 +0900

    update map display settings

--- app/src/main/java/net/uoneweb/android/mapboxtrial/MapFragment.kt
+++ app/src/main/java/net/uoneweb/android/mapboxtrial/MapFragment.kt
@@ -6,11 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.mapbox.maps.CameraOptions
-import com.mapbox.maps.MapView
-import com.mapbox.maps.Style
+import com.mapbox.geojson.Point
+import com.mapbox.maps.*
+import com.mapbox.maps.plugin.animation.flyTo
 import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
 import com.mapbox.maps.plugin.locationcomponent.location
+import com.mapbox.maps.plugin.locationcomponent.location2
+import com.mapbox.maps.plugin.viewport.data.FollowPuckViewportStateBearing
+import com.mapbox.maps.plugin.viewport.data.FollowPuckViewportStateOptions
+import com.mapbox.maps.plugin.viewport.state.FollowPuckViewportState
+import com.mapbox.maps.plugin.viewport.viewport
 import net.uoneweb.android.mapboxtrial.databinding.FragmentMapBinding
 
 class MapFragment : Fragment() {
@@ -38,29 +44,33 @@ class MapFragment : Fragment() {
         }
     }
 
-    private val onIndicatorBearingChangedListener = OnIndicatorBearingChangedListener { bearing ->
-        binding?.mapView?.getMapboxMap()?.setCamera(
-            CameraOptions.Builder().bearing(bearing).build()
-        )
-    }
-
-
     private fun initializeMapView(mapView: MapView) {
         mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS) {
             mapView.location.updateSettings {
                 enabled = true
+                this.pulsingEnabled = false
             }
-            mapView.location.addOnIndicatorBearingChangedListener() {
-                Log.d("", String.format("OnIndicatorBearingChangedListener %s", it))
+            mapView.location2.updateSettings2 {
+                showAccuracyRing = true
+                puckBearingEnabled = true
             }
-            mapView.location.addOnIndicatorPositionChangedListener() { point ->
-                Log.d("", String.format("OnIndicatorPositionChangedListener %s", point))
-                mapView.getMapboxMap().setCamera(CameraOptions.Builder().center(point).build())
-            }
-        }
 
-        /*
-        val viewportPlugin = mapView?.viewport
+            mapView.getMapboxMap().flyTo(
+                CameraOptions.Builder()
+                    .bearing(0.0)
+                    .center(Point.fromLngLat(139.76269, 35.67991))
+                    .zoom(12.0)
+                    .build()
+            )
+        }
+        setTrackingMode(false)
+        registerEventObserver(mapView)
+
+        //setupViewportPlugin(mapView)
+    }
+
+    private fun setupViewportPlugin(mapView: MapView) {
+        val viewportPlugin = mapView.viewport
         val followPuckViewportState: FollowPuckViewportState =
             viewportPlugin.makeFollowPuckViewportState(
                 FollowPuckViewportStateOptions.Builder()
@@ -72,7 +82,52 @@ class MapFragment : Fragment() {
 
         viewportPlugin.transitionTo(followPuckViewportState) { success ->
         }
-         */
+    }
+
+    private fun registerEventObserver(mapView: MapView) {
+        mapView.getMapboxMap().subscribe(
+            { event -> Log.d("MapEvents", event.type) },
+            listOf(
+                MapEvents.MAP_LOADED,
+                MapEvents.MAP_IDLE,
+                //MapEvents.RENDER_FRAME_STARTED,
+                //MapEvents.RENDER_FRAME_FINISHED,
+                MapEvents.CAMERA_CHANGED,
+                MapEvents.STYLE_DATA_LOADED,
+                MapEvents.SOURCE_DATA_LOADED
+            )
+        )
+    }
+
+    private val onIndicatorBearingChangedListener = OnIndicatorBearingChangedListener { bearing ->
+        binding?.mapView?.getMapboxMap()?.setCamera(
+            CameraOptions.Builder().bearing(bearing).build()
+        )
+    }
+
+    private val onIndicatorPositionChangedListener = OnIndicatorPositionChangedListener { point ->
+        binding?.mapView?.getMapboxMap()?.setCamera(CameraOptions.Builder().center(point).build())
+    }
+
+    private fun setTrackingMode(enable: Boolean) {
+        binding?.mapView?.let { mapView ->
+            mapView.location.removeOnIndicatorBearingChangedListener(
+                onIndicatorBearingChangedListener
+            )
+            mapView.location.removeOnIndicatorPositionChangedListener(
+                onIndicatorPositionChangedListener
+            )
+
+            if (enable) {
+                mapView.location.addOnIndicatorBearingChangedListener(
+                    onIndicatorBearingChangedListener
+                )
+
+                mapView.location.addOnIndicatorPositionChangedListener(
+                    onIndicatorPositionChangedListener
+                )
+            }
+        }
     }
 
     override fun onStart() {
